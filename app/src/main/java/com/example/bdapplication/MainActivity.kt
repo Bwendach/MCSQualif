d4b35dcff5e584083f97f1575f8904ddf88109b5
Bwendach <82569226+Bwendach@users.noreply.github.com>
2025-02-18 14:05:11 +0700

    first commit

---
+++ app/src/main/java/com/example/bdapplication/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.bdapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tvRegisterNow : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        etUsername = findViewById(R.id.et_username)
+        etPassword = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login)
+        tvRegisterNow = findViewById(R.id.tv_register_now)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text
+            val password = etPassword.text
+
+            if(username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+            Log.i("LOGIN_USERNAME", username.toString())
+            Log.i("LOGIN_PASSWORD", password.toString())
+        }
+
+        tvRegisterNow.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
